50127146b54c8c9e80a5b6ece5ef76ce586a1014
scarlett.hui <scarletthui225@gmail.com>
2018-06-10 01:11:12 +0800

    feat: add router and pages

---
+++ src/pages/Calculate.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CalculatePage extends Component {
+  render() {
+    return (
+      <h3>Hey calculate page</h3>
+    );
+  }
+}
+
+export default CalculatePage;
---
+++ src/pages/Todo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TodoPage extends Component {
+  render() {
+    return (
+      <h3>Hey todo page</h3>
+    );
+  }
+}
+
+export default TodoPage;
